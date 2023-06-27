06e76b25ea9a7b37a98100240057071ad2d6c7d2
Josseth Sanchez <128737955+Aine20@users.noreply.github.com>
2023-06-27 02:39:54 +0000

    Creating my Random Card Generator

--- src/app.js
+++ src/app.js
@@ -1,11 +1,32 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+window.onload = () => {
+  let randomSymbol = generateRandomSymbol();
+  document.getElementById("topSymbol").innerHTML = randomSymbol;
+  document.getElementById("bottomSymbol").innerHTML = randomSymbol;
+  document.getElementById("number").textContent = generateRandomNumber();
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let symbolElements = document.querySelectorAll("#topSymbol, #bottomSymbol");
+  for (let i = 0; i < symbolElements.length; i++) {
+    let symbol = symbolElements[i].textContent;
+    if (symbol === "♦️" || symbol === "♥️") {
+      symbolElements[i].classList.add("redSymbol");
+    }
+  }
 };
+
+let generateRandomNumber = () => {
+  let numbers = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+  let indexNumbers = Math.floor(Math.random() * numbers.length);
+  return numbers[indexNumbers];
+};
+
+let generateRandomSymbol = () => {
+  let symbols = ["♦️", "♠️", "♥️", "♣️"];
+  let indexSymbol = Math.floor(Math.random() * symbols.length);
+  return symbols[indexSymbol];
+};
+
+
+
+
+
